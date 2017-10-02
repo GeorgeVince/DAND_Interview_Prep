a0ee5e1d2c5c8f65bef3a0a0530457f7eefcd480
George Vince <george.vince@hotmail.co.uk>
2017-10-02 18:35:04 +0100

    Initial commit

---
+++ Fundementals/index_capitalization.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  2 18:33:13 2017
+
+@author: georg
+
+Given a string and an array of integers representing indices, capitalize all 
+letters at the given indices.
+
+For example:
+
+    capitalize("abcdef",[1,2,5]) = "aBCdeF"
+    capitalize("abcdef",[1,2,5,100]) = "aBCdeF". There is no index 100.
+
+The input will be a lowercase string with no spaces and an array of digits.
+
+Good luck!
+
+
+"""
+
+def capitalize(s,ind):
+   return s
+
+
+print (capitalize("abcdef",[1,2,5]))
